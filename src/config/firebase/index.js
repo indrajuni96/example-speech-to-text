be32bc66bac655f77ee6eca9ff8ec7cce793af5b
indrajuni96 <33251585+indrajuni96@users.noreply.github.com>
2020-09-27 09:30:45 +0700

    setup firebase

---
+++ src/config/firebase/index.js
@@ -0,0 +1,13 @@
+import firebase from "firebase"
+
+const Firebase = firebase.initializeApp({
+  apiKey: "AIzaSyBmGzQ-GsbTRHiq2r-jQLJmRKxr0QqvsUY",
+  authDomain: "example-speech-81c9a.firebaseapp.com",
+  databaseURL: "https://example-speech-81c9a.firebaseio.com",
+  projectId: "example-speech-81c9a",
+  storageBucket: "example-speech-81c9a.appspot.com",
+  messagingSenderId: "410202156599",
+  appId: "1:410202156599:web:450814e15c2727b1ce19b2"
+})
+
+export default Firebase
