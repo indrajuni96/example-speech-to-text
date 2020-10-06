d6fdeb4b22c18639336cc1029ecab3818815de6f
indrajuni96 <33251585+indrajuni96@users.noreply.github.com>
2020-10-06 08:34:59 +0700

    validasi form register

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { ScrollView, StatusBar, Text, View, TouchableOpacity } from 'react-native'
+import { ScrollView, StatusBar, Text, View, TouchableWithoutFeedback } from 'react-native'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
 
 import { colors, ConfigBackHandler } from '../../utils'
 import { Input, Loading, Space, ErrorMessage } from '../../components'
 import styles from './styles'
 
 export default function Register() {
+  const textErrorMessage = 'Wajib Diisi'
+
+  const onSubmit = (values) => {
+    console.log(values)
+  }
+
   return (
     <>
       <StatusBar backgroundColor="white" barStyle="dark-content" />
@@ -18,25 +26,58 @@ export default function Register() {
         <Space valSpace={50} />
 
         <View style={styles.wrapperMain}>
-          <Input
-            label="Email"
-          />
-          <ErrorMessage />
-
-          <Input
-            label="password"
-            secureTextEntry
-          />
-          <ErrorMessage />
-
-          <Space valSpace={24} />
-          <TouchableOpacity
-            style={styles.button}
-          // onPress={handleSubmit}
+          <Formik
+            initialValues={{
+              email: '',
+              password: ''
+            }}
+            validationSchema={Yup.object({
+              email: Yup.string().required(textErrorMessage).trim(textErrorMessage).email('format harus email'),
+              password: Yup.string().required(textErrorMessage).trim(textErrorMessage).min(6, 'minimal 6 karakter')
+            })}
+            onSubmit={onSubmit}
           >
-            <Text style={styles.textButton}>Login</Text>
-          </TouchableOpacity>
-          <Space valSpace={24} />
+            {({ handleChange, handleBlur, handleSubmit, values, touched, errors, isSubmitting }) => {
+              const disable = values.email && values.password ? false : true
+              const opacity = values.email && values.password ? 1 : 0.3
+              return (
+                <>
+                  <Input
+                    label="Email"
+                    value={values.email}
+                    errors={errors.email}
+                    touched={touched.email}
+                    onChangeText={handleChange('email')}
+                    onBlur={handleBlur('email')}
+                  />
+                  <ErrorMessage touched={touched.email} errors={errors.email} />
+
+                  <Input
+                    label="Password"
+                    secureTextEntry
+                    value={values.password}
+                    errors={errors.password}
+                    touched={touched.password}
+                    onChangeText={handleChange('password')}
+                    onBlur={handleBlur('password')}
+                  />
+                  <ErrorMessage touched={touched.password} errors={errors.password} />
+
+                  <Space valSpace={24} />
+                  <TouchableWithoutFeedback
+                    disabled={disable}
+                    onPress={handleSubmit}
+                  >
+                    <View style={[styles.button, { opacity }]}>
+                      <Text style={styles.textButton}>Login</Text>
+                    </View>
+                  </TouchableWithoutFeedback>
+                  <Space valSpace={24} />
+                </>
+              )
+            }
+            }
+          </Formik>
         </View>
 
       </View>
