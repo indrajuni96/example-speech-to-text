444d0156ebdd4493cf19f157bd4e9d91bdf45a28
indrajuni96 <33251585+indrajuni96@users.noreply.github.com>
2021-01-13 16:36:38 +0700

    added isLoading register user

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -9,6 +9,21 @@ const initialState = {
 
 export default function Auth(state = initialState, action) {
   switch (action.type) {
+    case `${types.REGISTER_USER}_PENDING`:
+      return {
+        ...state,
+        isLoading: true
+      }
+    case `${types.REGISTER_USER}_FULFILLED`:
+      return {
+        ...state,
+        isLoading: false,
+      }
+    case `${types.REGISTER_USER}_REJECTED`:
+      return {
+        ...state,
+        isLoading: false
+      }
     case `${types.LOGIN_USER}_PENDING`:
       return {
         ...state,
